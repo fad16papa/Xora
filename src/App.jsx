fc7e038d5954700b6304e70e8ce26879967ad20c
fad16papa <fad16papa@gmail.com>
2024-09-21 18:20:11 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+
+const App = () => {
+  return (
+    <div className="overflow-hidden">
+      <Header />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
