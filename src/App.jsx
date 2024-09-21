7d0b84c62f33b9b369cc020cdbd41b1903084c2c
fad16papa <fad16papa@gmail.com>
2024-09-21 23:59:19 +0800

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Features from "./sections/Features";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 
@@ -6,6 +7,7 @@ const App = () => {
     <div className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
     </div>
   );
 };
